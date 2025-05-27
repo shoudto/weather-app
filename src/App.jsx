4f0d08eb599e61a7dbf538e088bac8cb4a6b2941
Oscar Shoudt <shoudto@Oscars-Air.home>
2025-05-26 20:33:07 -0400

    fix tailwind import and added a header

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // Later trigger fetch here.
+    console.log("search for:", city);
+  };
+
   return (
-    <div className="text-3xl font-bold text-blue-500">
-      <h1>Hello Tailwind</h1>
+    <div className="min-h-screen bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white p-4">
+      {/* HEADER */}
+
+      <header className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Weather Dashboard</h1>
+        <button className="text-sm bg-gray-300 px-3 py-1 rounded">
+          Dark mode
+        </button>
+      </header>
     </div>
   );
 }
